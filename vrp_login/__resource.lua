1e88abcf0d3904e988ba03601f8e24ac2e9efc4f
Combat <lcombat09@gmail.com>
2020-08-18 11:35:06 -0300

    Loguin att 18.08.20

---
+++ vrp_login/__resource.lua
@@ -0,0 +1,20 @@
+resource_manifest_version '44febabe-d386-4d18-afbe-5e627f4af937'
+
+ui_page "nui/ui.html"
+
+client_scripts {
+	"@vrp/lib/utils.lua",
+	"client.lua"
+}
+
+files {
+	"nui/ui.html",
+	"nui/ui.js",
+	"nui/ui.css".
+	"nui/img/metro.png",
+	"nui/img/garagem.png",
+	"nui/img/garagemss.png",
+	"nui/img/garagempb.png",
+	"nui/img/hp.png",
+	"nui/img/aero.png"  
+}
