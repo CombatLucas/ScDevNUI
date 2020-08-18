ec529f37c1e94a7aef0ccfae39a791fcc3407c76
Combat <lcombat09@gmail.com>
2020-08-18 11:34:34 -0300

    Pescador att 18.08.20

---
+++ nav_pescador/__resource.lua
@@ -0,0 +1,28 @@
+resource_manifest_version '44febabe-d386-4d18-afbe-5e627f4af937'
+
+ui_page "nui/ui.html"
+
+client_scripts {
+	"@vrp/lib/utils.lua",
+	"client.lua"
+}
+
+server_scripts {
+	"@vrp/lib/utils.lua",
+	"server.lua"
+}
+
+files {
+	"nui/ui.html",
+	"nui/ui.js",
+	"nui/ui.css",
+	"nui/corvina.png",
+	"nui/dourado.png",
+	"nui/lambari.png",
+	"nui/pacu.png",
+	"nui/pintado.png",
+	"nui/pirarucu.png",
+	"nui/salmao.png",
+	"nui/tilapia.png",
+	"nui/tucunare.png"
+}
